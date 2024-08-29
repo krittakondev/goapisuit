d188872f760abbdc0a0cfca3841311e1f3c0ba7b
krittakondev <krit@9krit.dev>
2024-08-29 17:53:57 +0700

    draft: template

--- cmd/heykrit/main.go
+++ cmd/heykrit/main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
 
-func main(){
-	fmt.Println("hello krit")
+	"github.com/krittakondev/goapisuit/pkg/maketemplate"
+)
+
+func capitalizeFirstChar(s string) string {
+	if len(s) == 0 {
+		return s
+	}
+	return strings.ToUpper(string(s[0])) + s[1:]
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("You need to pass a command")
+		os.Exit(1)
+	}
+
+	command := os.Args[1]
+
+	switch command {
+	case "make":
+		if len(os.Args) < 3 {
+			fmt.Println("Please Enter Route name")
+			os.Exit(1)
+		}
+		routeName := capitalizeFirstChar(os.Args[2])
+
+		mkroute := &maketemplate.MakeRoute{
+			Name: routeName,
+		}
+		if err := mkroute.New(); err != nil{
+			log.Fatal(err)
+		}
+
+	default:
+		fmt.Println("Unknown command:", command)
+		os.Exit(1)
+	}
 }
--- pkg/maketemplate/createModel.go
+++ pkg/maketemplate/createModel.go
@@ -1,5 +1,23 @@
 package maketemplate
 
-type Model struct{
-	
+import (
+    "os"
+    "text/template"
+)
+
+
+type MakeRoute struct{
+	Name string
+}
+
+func (mr  *MakeRoute) New() error{
+	tmpl, err := template.ParseFiles("pkg/maketemplate/tmpl/route.go.tmpl")
+	if err != nil{
+		return err
+	}
+	if err := tmpl.Execute(os.Stdout, mr); err != nil{
+		return err
+	}
+
+	return nil
 }
