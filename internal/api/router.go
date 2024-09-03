9c062ddc4dff49ae3d0c7be3d4428242bf5ad5e8
krittakondev <krit@9krit.dev>
2024-09-04 00:49:31 +0700

    update main router

--- internal/api/router.go
+++ internal/api/router.go
@@ -1,17 +1,22 @@
 package routes
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/gorm"
+)
 
-type Response struct{
-	Code int
-	Message string
-	Data interface{}
+type Response struct {
+	Code    int         `json:"code"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data"`
 }
 
 type Route struct{
+	DB *gorm.DB
+	PageLimit int
 }
 
-func (r *Route) MainRoute(c *fiber.Ctx) error{
+func (r *Route) Index_get(c *fiber.Ctx) error{
 	
 	resp := Response{
 		Message: "hello goapisuit",
