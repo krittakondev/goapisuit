5c6fdcea11c281dd56f50c07c9045ec346ceb400
krittakondev <krit@9krit.dev>
2024-09-04 19:40:10 +0700

    feat: create model

--- pkg/maketemplate/createModel.go
+++ pkg/maketemplate/createModel.go
@@ -12,24 +12,37 @@ type MakeRoute struct{
 }
 
 func (mr  *MakeRoute) New() error{
-	tmpl, err := template.ParseFiles("pkg/maketemplate/tmpl/route.go.tmpl")
+	tmplRoute, err := template.ParseFiles("pkg/maketemplate/tmpl/route.go.tmpl")
 	if err != nil{
 		return err
 	}
-	createPath := "internal/api/"+mr.Name+".go"
+	tmplModel, err := template.ParseFiles("pkg/maketemplate/tmpl/model.go.tmpl")
+	if err != nil{
+		return err
+	}
+	createPathRoute := "internal/api/"+mr.Name+".go"
+	createPathModel := "internal/models/"+mr.Name+".go"
 
-	if _, err := os.Stat(createPath); err == nil {
-		log.Fatal(createPath+ " is Exist")
+	if _, err := os.Stat(createPathRoute); err == nil{
+		log.Fatal(createPathRoute+ " is Exist")
+	}
+	if _, err := os.Stat(createPathModel); err == nil{
+		log.Fatal(createPathModel+ " is Exist")
 	}
 
-
-	file, err := os.OpenFile(createPath, os.O_CREATE|os.O_WRONLY, 0644)
+	file, err := os.OpenFile(createPathRoute, os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		log.Fatal(err)
 	}
-	
+	if err := tmplRoute.Execute(file, mr); err != nil{
+		return err
+	}
 
-	if err := tmpl.Execute(file, mr); err != nil{
+	file, err = os.OpenFile(createPathModel, os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := tmplModel.Execute(file, mr); err != nil{
 		return err
 	}
 
