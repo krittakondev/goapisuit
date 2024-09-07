417f10ebc3a71e98c423ccbb8a5e798e266bd9c9
krittakondev <krit@9krit.dev>
2024-09-08 02:19:55 +0700

    feat: jwt auth

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/andybalholm/brotli v1.0.5 // indirect
 	github.com/go-sql-driver/mysql v1.8.1 // indirect
 	github.com/gofiber/fiber/v2 v2.52.5 // indirect
+	github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
 	github.com/google/uuid v1.5.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
---
+++ internal/middlewares/auth.go
@@ -0,0 +1,39 @@
+package middlewares
+
+import (
+	"errors"
+
+	"fmt"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+
+func RequireJwtAuth(c *fiber.Ctx) error {
+	jwt_secret := os.Getenv("JWT_SECRET")
+
+	reqtoken := c.GetReqHeaders()["Token"]
+	if reqtoken == nil {
+		return errors.New("Not found Token Header")
+	}
+
+	token, err := jwt.Parse(reqtoken[0], func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("There was an error in parsing")
+		}
+		return []byte(jwt_secret), nil
+	})
+
+	if err != nil {
+		return err
+	}
+	if token == nil {
+		return errors.New("invalid token")
+	}
+
+	c.Locals("user", token.Claims)
+
+	return nil
+}
