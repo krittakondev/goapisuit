bd3c62f66be13ac6000565f097b8609e86cb1bb5
krittakondev <krit@9krit.dev>
2024-10-10 02:27:58 +0700

    tests: add testcase strings

--- pkg/utils/strings_test.go
+++ pkg/utils/strings_test.go
@@ -1,6 +1,8 @@
 package utils
 
-import "testing"
+import (
+	"testing"
+)
 
 func Test_KebabToCamel(t *testing.T) {
 	type args struct {
@@ -17,8 +19,16 @@ func Test_KebabToCamel(t *testing.T) {
 			want: "TestCase",
 		},
 		{
+			args: "test-case-some",
+			want: "TestCaseSome",
+		},
+		{
 			args: "Test-Case",
 			want: "TestCase",
+		},
+		{
+			args: "Test-Case-some",
+			want: "TestCaseSome",
 		},
 		{
 			args: "TEST-CASE",
@@ -37,6 +47,42 @@ func Test_KebabToCamel(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := KebabToCamel(tt.args); got != tt.want {
 				t.Errorf("kebabToCamel() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestCamelToKebab(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		// TODO: Add test cases.
+		{
+			args: args{s: "testcase"},
+			want: "testcase",
+		},
+		{
+			args: args{s: "TestCase"},
+			want: "test-case",
+		},
+		{
+			args: args{s: "TestCaseSome"},
+			want: "test-case-some",
+		},
+		{
+			args: args{s: "TestCaseSome"},
+			want: "test-case-some",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := CamelToKebab(tt.args.s); got != tt.want {
+				t.Errorf("CamelToKebab() = %v, want %v", got, tt.want)
 			}
 		})
 	}
