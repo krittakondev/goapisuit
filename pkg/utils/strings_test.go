0724b7e9c587c17458cf161b4b5efe78b78eee96
krittakondev <krit@9krit.dev>
2024-10-10 01:45:12 +0700

    test: strings utils

---
+++ pkg/utils/strings_test.go
@@ -0,0 +1,35 @@
+package utils
+
+import "testing"
+
+func Test_KebabToCamel(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args string
+		want string
+	}{
+		// TODO: Add test cases.
+		{
+			args: "test-case",
+			want: "TestCase",
+		},
+		{
+			args: "Test-Case",
+			want: "TestCase",
+		},
+		{
+			args: "TEST-CASE",
+			want: "TestCase",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := KebabToCamel(tt.args); got != tt.want {
+				t.Errorf("kebabToCamel() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
